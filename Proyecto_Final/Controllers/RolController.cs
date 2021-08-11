766ec70e90eca65fb034fc143e645e15fdea48fe
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-08-11 09:22:24 -0500

    creacion de Edit y Delete en la clase rol

--- Proyecto_Final/Controllers/RolController.cs
+++ Proyecto_Final/Controllers/RolController.cs
@@ -57,8 +57,76 @@ namespace Proyecto_Final.Controllers
             }
         }
 
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findRol = db.roles.Find(id);
+                    db.roles.Remove(findRol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+
+            }
+
+
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    roles findrol = db.roles.Where(a => a.id == id).FirstOrDefault();
+                    return View(findrol);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(roles editroles)
+        {
+            try
+            {
+
+                using (var db = new inventario2021Entities())
+                {
+                    roles rol = db.roles.Find(editroles.id);
+
+                    rol.descripcion = editroles.descripcion;
+                    
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+
 
     }
-
-
 }
