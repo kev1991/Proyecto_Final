4b908f26c9683579ecb902b54cc0ac6fbf66ac55
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-08-11 08:59:21 -0500

    creacion del Index en la  clase rol

---
+++ Proyecto_Final/Controllers/RolController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Final.Models;
+
+namespace Proyecto_Final.Controllers
+{
+    public class RolController : Controller
+    {
+        // GET: Rol
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.roles.ToList());
+            }
+                
+        }
+    }
+}
