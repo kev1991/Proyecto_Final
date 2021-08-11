8e7e5feb5ac4fc41bafd545b24b982bcef6e43c0
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-08-11 10:52:09 -0500

    creacion del crud de la clase proveedor

---
+++ Proyecto_Final/Controllers/ProveedorController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Final.Models;
+
+
+namespace Proyecto_Final.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.proveedor.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findproveedor = db.proveedor.Find(id);
+                return View(findproveedor);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findproveedor = db.proveedor.Find(id);
+                    db.proveedor.Remove(findproveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor findproveedor = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(findproveedor);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(proveedor editproveedor)
+        {
+            try
+            {
+
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor prove = db.proveedor.Find(editproveedor.id);
+
+                    prove.nombre = editproveedor.nombre;
+                    prove.direccion = editproveedor.direccion;
+                    prove.telefono = editproveedor.telefono;
+                    prove.nombre_contacto = editproveedor.nombre_contacto;
+                    
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
