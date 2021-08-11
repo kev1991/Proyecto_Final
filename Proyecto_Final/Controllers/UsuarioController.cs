59f6be1867d52fa1c2f09caf3011cd8798a2eec8
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-08-11 08:48:48 -0500

    creacion de Edit de la clase Usuario

--- Proyecto_Final/Controllers/UsuarioController.cs
+++ Proyecto_Final/Controllers/UsuarioController.cs
@@ -76,5 +76,54 @@ namespace Proyecto_Final.Controllers
             }
         }
 
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario findUser = db.usuario.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(usuario editUser)
+        {
+            try
+            {
+
+                using (var db = new inventario2021Entities())
+                {
+                    usuario user = db.usuario.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.apellido = editUser.apellido;
+                    user.email = editUser.email;
+                    user.fecha_nacimiento = editUser.fecha_nacimiento;
+                    user.password = editUser.password;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
     }
+
 }
