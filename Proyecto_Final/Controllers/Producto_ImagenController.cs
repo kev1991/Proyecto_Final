ef5eed4b04abd8cfc85c4fb69ca56bd102a523d5
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-08-17 22:09:14 -0500

    #1- Creación del crud de la tabla producto_imagen. #2-menu desplegable para usar todas las vistas de las tablas creadas asta el momento. #3-modificamos el indesx de las tablas creadas por el momento con una finción de JS que nos deja confirmar antes de eliminar algo

---
+++ Proyecto_Final/Controllers/Producto_ImagenController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Final.Models;
+
+namespace Proyecto_Final.Controllers
+{
+    public class Producto_ImagenController : Controller
+    {
+        // GET: Producto_Imagen
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto_imagen.ToList());
+            }
+        }
+
+        public static string NombreProducto(int idproducto)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.producto.Find(idproducto).nombre;
+            }
+        }
+
+        public ActionResult ListarProducto()
+        {
+            using (var db = new inventario2021Entities())
+                return PartialView(db.producto.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto_imagen productoImg)
+        {
+            if (!ModelState.IsValid)
+
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.producto_imagen.Add(productoImg);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+                throw;
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto_imagen.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                producto_imagen producto_Image_Edit = db.producto_imagen.Where(kev => kev.id == id).FirstOrDefault();
+                return View(producto_Image_Edit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+
+        public ActionResult Edit(producto_imagen producto_Imagen_Edit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var Product_Img = db.producto_imagen.Find(producto_Imagen_Edit.id);
+                    Product_Img.imagen = producto_Imagen_Edit.imagen;
+                    Product_Img.id_producto = producto_Imagen_Edit.id_producto;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception kev)
+            {
+                ModelState.AddModelError("", "error" + kev);
+                return View();
+                throw;
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto_imagen producto_Image = db.producto_imagen.Find(id);
+                    db.producto_imagen.Remove(producto_Image);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
