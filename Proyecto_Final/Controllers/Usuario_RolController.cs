dbe3318122a53d4d0b915d21b5b010a5f619eac6
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-08-22 18:23:18 -0500

    creación del crud de usuariol
    doy po rterminado la creación de todas las tablas

--- Proyecto_Final/Controllers/Usuario_RolController.cs
+++ Proyecto_Final/Controllers/Usuario_RolController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto_Final.Models;
+
 
 namespace Proyecto_Final.Controllers
 {
@@ -11,7 +13,130 @@ namespace Proyecto_Final.Controllers
         // GET: Usuario_Rol
         public ActionResult Index()
         {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.ToList());
+            }
+
+        }
+
+        public static string Nombre_Usuario(int idUsuario)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(idUsuario).nombre;
+            }
+        }
+
+        public static string Descripcion_Rol(int idRol)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.roles.Find(idRol).descripcion;
+            }
+        }
+
+        public ActionResult ListarUsuario()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public ActionResult ListarRol()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.roles.ToList());
+            }
+        }
+
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuariorol usuario_rol)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.usuariorol.Add(usuario_rol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception kev)
+            {
+                ModelState.AddModelError("", "error " + kev);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuariorol.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                usuariorol usuario_rol_Edit = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                return View(usuario_rol_Edit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuariorol usuario_rol_Edit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var old_usuario_rol = db.usuariorol.Find(usuario_rol_Edit.id);
+                    old_usuario_rol.idUsuario = usuario_rol_Edit.idUsuario;
+                    old_usuario_rol.idRol = usuario_rol_Edit.idRol;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception kev)
+            {
+                ModelState.AddModelError("", "error " + kev);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuariorol usuario_rol = db.usuariorol.Find(id);
+                    db.usuariorol.Remove(usuario_rol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception kev)
+            {
+                ModelState.AddModelError("", "error " + kev);
+                return View();
+            }
         }
     }
 }
