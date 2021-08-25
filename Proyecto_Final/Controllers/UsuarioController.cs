41d7d471b18f41b18de885765401aa383d7ab8ce
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-08-25 18:27:59 -0500

    creación del login

--- Proyecto_Final/Controllers/UsuarioController.cs
+++ Proyecto_Final/Controllers/UsuarioController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Proyecto_Final.Models;
+using System.Web.Security;
+using System.Text;
+using System.Web.Security;
 
 namespace Proyecto_Final.Controllers
 {
+   
     public class UsuarioController : Controller
     {
+        [Authorize]
         // GET: Usuario
         public ActionResult Index()
         {
@@ -35,6 +40,7 @@ namespace Proyecto_Final.Controllers
             {
                 using (var db = new inventario2021Entities())
                 {
+                    usuario.password = UsuarioController.HashSHA1(usuario.password);
                     db.usuario.Add(usuario);
                     db.SaveChanges();
                     return RedirectToAction("Index");
@@ -45,6 +51,20 @@ namespace Proyecto_Final.Controllers
                 ModelState.AddModelError("", "error " + ex);
                 return View();
             }
+        }
+
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
 
 
@@ -124,6 +144,55 @@ namespace Proyecto_Final.Controllers
                 return View();
             }
         }
+
+        public ActionResult Login(string mensaje = "")
+        {
+            ViewBag.Message = mensaje;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string user, string password)
+        {
+            try
+            {
+                string passEncrip = UsuarioController.HashSHA1(password);
+                using (var db = new inventario2021Entities())
+                {
+                    var userLogin = db.usuario.FirstOrDefault(e => e.email == user && e.password == passEncrip);
+                    if (userLogin != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(userLogin.email, true);
+                        Session["User"] = userLogin;
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return Login("Verifique sus datos");
+                    }
+                }
+            }
+            catch (Exception kev)
+            {
+                ModelState.AddModelError("", "error " + kev);
+                return View();
+            }
+        }
+
+        [Authorize]
+        public ActionResult CloseSession()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+
+
     }
+
+
 
 }
